c1fd4bcde1e12667dbedaf2b1002fadf5b0dea50
htayaung <htayaung.mm45@gmail.com>
2018-10-07 18:42:46 +0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetcore_starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
